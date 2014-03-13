abf6694d3047d13aa6732061f591980f5ee2b8d3
Chris Rawles <crawles@gmail.com>
2014-03-13 18:44:33 -0500

    Working onTAN

--- hw2/train.py
+++ hw2/train.py
@@ -6,27 +6,36 @@ class Train():
         self.dataset = dataset
         self.nodes = data.Nodes()
 
-    def calc_all_naive(self):
-        len_all = 0
-        for att in self.dataset.attr_val_set.values():
-            print att
-            len_all = len_all + len(att)
-        return (len_all-2)
+    #def calc_all_naive(self):
+    #    len_all = 0
+    #    for att in self.dataset.attr_val_set.values():
+    #        print att
+    #        len_all = len_all + len(att)
+    #    return (len_all-2)
 
+    def laplace(self,key_vals):
+        """ for numerator: 
+            simulate P(X1,X2...) if NO data 
+        """
+        att_keys = key_vals.keys()
+        result = 1
+        for key in att_keys:
+            result = result*len(self.dataset.attr_val_set[key])
+        return result
+        
     def calc_P(self,Xx,Yy):
         """ P(Xx|Yy) = P(Xx,Yy)/P(Yy)  e.g ({'no_of_nodes_in':'5'},{'class':'metastases'}) """
         #print Xx,Yy
         total = float(len(self.dataset))
         if Yy:
-            #conditional
-            lap = len(self.dataset.attr_val_set[Xx.keys()[0]])
-            num_Yy = total - self.key_val_not_in_dataset(Yy) + lap
+            #conditioned
+            laplace = self.laplace(Xx)
+            num_Yy = total - self.key_val_not_in_dataset(Yy) + laplace
         else:
             #not conditioned. i.e P(Yy)
             num_Yy = total + 2 #2 classes
             Yy = {}
         num_Xx = total - self.key_val_not_in_dataset(dict(Xx.items() + Yy.items()))
-        #TODO check this
         return (num_Xx+1)/(num_Yy) #laplace
 
     def key_val_not_in_dataset(self,key_vals):
@@ -51,14 +60,14 @@ class Train():
             #P(Yy)
             self.nodes[('class',Yy)] = self.calc_P({'class':Yy},None)
             for X in attr_keys:
+                #sub_attr_keys = attr_keys 
+                #sub_attr_keys.remove(X)
                 for Xx in self.dataset.attr_val_set[X]:
-                    #P(Xx|Yy)
-                    self.nodes[(X,Xx)][('class',Yy)] = self.calc_P({X:Xx},{'class':Yy}) 
+                    self.nodes[(X,Xx)][('class',Yy)] = self.calc_P({X:Xx},{'class':Yy}) #P(Xx|Yy)
                     for X1 in attr_keys:
                         for Xx1 in self.dataset.attr_val_set[X1]:
-                            #P(Xx,Xx1,|Yy)
                             #TODO need this for TAN
-                            #self.nodes[((X,Xx),(X1,Xx1))][('class',Yy)] = self.calc_P({X:Xx},{X:Xx1,'class':Yy}) 
+                            #self.nodes[((X,Xx),(X1,Xx1))][('class',Yy)] = self.calc_P({X:Xx},{X:Xx1,'class':Yy}) #P(Xx,Xx1,|Yy)
                             pass
                     
 
